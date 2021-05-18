4f69815d9afc41f9e16e746c561dcc32b0cc864f
FELIPEVARJAO86 <82450219+FELIPEVARJAO86@users.noreply.github.com>
2021-05-18 07:26:42 -0300

    Create index.php

---
+++ av1/index.php
@@ -0,0 +1,29 @@
+<! DOCTYPE html>
+< html >
+	< cabeça >
+		< title > PHP bruta </ title >
+		< Vínculo  rel = " stylesheet "  href = " https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css "  integridade = " SHA384-Gn5384xqQ1aoWXA + 058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW / dAiS6JXm "  crossorigin = " anonymous " >
+	</ head >
+	< corpo >
+		< div  class = " container " >
+			
+			< div  class = " card text-center mt-5 " >
+				< div  class = " card-header " >
+					MENU ADMIN
+				</ div >
+				< div  class = " card-body " >
+					< A  href = " menu_cli.php "  class = " btn btn-primária " > Menu Cliente </ a >
+					< A  href = " menu_prod.php "  class = " btn btn-primária " > Menu Produto </ a >
+				</ div >
+				< div  class = " card-footer text-muted " >
+					AV1 3DAW - CRUD
+				</ div >
+			</ div >
+			
+		</ div >
+		
+		< Roteiro  src = " https://code.jquery.com/jquery-3.2.1.slim.min.js "  integridade = " SHA384-KJ3o2DKtIkvYIK3UENzmM7KCkRr / RE9 / Qpg6aAZGJwFDMVNA / GpGFF93hXpG5KkN "  crossorigin = " anonymous " > </ script de >
+		< Roteiro  src = " https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js "  integridade = " SHA384-ApNbgh9B + Y1QKtv3Rn7W3mgPxhU9K / ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q "  crossorigin = " anonymous " > </ script >
+		< Roteiro  src = " https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js "  integridade = " SHA384-JZR6Spejh4U02d8jOt6vLEHfe / JQGiRRSQQxSfFWpi1MquVdAyjUar5 + 76PVCmYl "  crossorigin = " anonymous " > </ script de >
+	</ body >
+</ html >
