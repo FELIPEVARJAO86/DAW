053e3ffb0bbae4d6ee2177ea0941c2fe5681e1f7
FELIPEVARJAO86 <82450219+FELIPEVARJAO86@users.noreply.github.com>
2021-07-20 07:57:39 -0300

    Create index.php

---
+++ AV2/index.php
@@ -0,0 +1,221 @@
+<! DOCTYPE html>
+< html  lang = " pt-br " >
+    < cabeça >
+        < meta  charset = " utf-8 " />
+        < meta  name = " viewport "  content = " width = device-width, initial-scale = 1, shrink-to-fit = no " />
+        < meta  name = " descrição "  content = " " />
+        < meta  name = " autor "  content = " " />
+        < title > Loja Ximbolé Bahiano </ title >
+        <! - CSS do tema central (inclui Bootstrap) ->
+        < link  href = " css / styles.css "  rel = " stylesheet " />
+    </ head >
+    < corpo >
+        <! - Navegação ->
+        < nav  class = " navbar navbar-expand-lg navbar-light bg-light " >
+            < div  class = " container px-4 px-lg-5 " >
+                < A  class = " navbar-brand "  href = " index.php " > Ximbolé Bahiano </ a >
+                < button  class = " navbar-toggler "  type = " button "  data-bs-toggle = " collapse "  data-bs-target = " #navbarSupportedContent "  aria-controls = " navbarSupportedContent "  aria- extended = " false "  aria-label = " Alternar navegação " > < span  class = " navbar-toggler-icon "> </ span> </ botão >
+                < div  class = " collapse navbar-collapse "  id = " navbarSupportedContent " >
+                    < ul  class = " navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4 " >
+                        < Li  class = " nav-ponto " > < a  class = " nav-link ativo "  aria-current = " página "  href = " index.php " > Home </ a > </ li >
+                        < Li  class = " nav-ponto " > < a  class = " nav-link "  href = " listaprod.php " > Lista dos Produtos </ a > </ li >
+                        < Li  class = " nav-ponto " > < a  class = " -link nav "  href = " buscar.php " > Buscar PRODUTO POR código de barra </ a > </ li >
+                        < Li  class = " nav-ponto " > < a  class = " nav-link "  href = " inserir.php " > Inserir Produto </ a > </ li >
+                    </ ul >
+                </ div >
+            </ div >
+        </ nav >
+        <! - Cabeçalho ->
+        < header  class = " bg-dark py-5 " >
+            < div  class = " container px-4 px-lg-5 my-5 " >
+                < div  class = " text-center text-white " >
+                    < h1  class = " display-4 fw-bolder " > Loja Ximbolé Bahiano </ h1 >
+                    < p  class = " lead fw-normal text-white-50 mb-0 " > Loja voltada para artigos tecnologicos </ p >
+                </ div >
+            </ div >
+        </ header >
+        <! - Seção ->
+        < section  class = " py-5 " >
+            < div  class = " container px-4 px-lg-5 mt-5 " >
+                < div  class = " row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center " >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000001.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000001.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Samsung Galaxy M21s </ h5 >
+                                        <! - Preço do produto ->
+                                        R $ 1.306,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000001.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000002.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000002.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Xiaomi Redmi 9 (Global) </ h5 >
+                                    <! - Preço do produto ->
+                                        R $ 1.190,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000002.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >    
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000003.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000003.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Smartphone Samsung Galaxy M12 </ h5 >
+                                        <! - Preço do produto ->
+                                        R $ 1.099,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000003.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000004.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000004.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Moto G9 Play </ h5 >
+                                        <! - Preço do produto ->
+                                        R $ 1.099,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000004.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000005.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000005.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Xiaomi Redmi Note 10 </ h5 >
+                                        <! - Preço do produto ->
+                                        R $ 1.725,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000005.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000006.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000006.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > LG K61 </ h5 >
+                                        <! - Preço do produto ->
+                                        $ 1.129,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000006.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000007.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  src = " imagens / 00000000007.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Smartphone Motorola Moto E7 Power </ h5 >
+                                        <! - Preço do produto ->
+                                        R $ 799,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000007.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                    < div  class = " col mb-5 " >
+                        < Um  estilo = " text-decoration: none; "  href = " 00000000001.php " >
+                            < div  class = " card h-100 " >
+                                <! - Imagem do produto ->
+                                < img  class = " card-img-top "  style = " height: 300px; largura: 150px; margem esquerda: 22% "  src = " imagens / 00000000008.png "  alt = " ... " />
+                                <! - Detalhes do produto ->
+                                < div  class = " card-body p-4 " >
+                                    < div  class = " text-center " >
+                                        <! - Nome do produto ->
+                                        < h5  class = " fw-bolder " > Samsung Galaxy M31 </ h5 >
+                                        <! - Preço do produto ->
+                                        R $ 1.712,00
+                                    </ div >
+                                </ div >
+                                <! - Ações do produto ->
+                                < div  class = " card-footer p-4 pt-0 border-top-0 bg-transparent " >
+                                    < Div  class = " text-center " > < a  class = " btn btn-outline-escuro mt-auto "  href = " 00000000008.php " > Ver Produto </ a > </ div >
+                                </ div >
+                            </ div >
+                        </ a >
+                    </ div >
+                </ div >
+            </ div >
+        </ seção >
+        <! - Rodapé ->
+        < footer  class = " py-5 bg-dark " >
+            < div  class = " container " > < p  class = " m-0 text-center text-white " > Direitos autorais e cópia ; Seu site 2021 </ p > </ div >
+        </ rodapé >
+        <! - Bootstrap core JS ->
+        < script  src = " https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js " > </ script >
+        <! - Tema principal JS ->
+        < script  src = " js / scripts.js " > </ script >
+    </ body >
+</ html >
